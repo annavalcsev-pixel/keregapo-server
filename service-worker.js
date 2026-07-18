605ca59fbd9a8a314f32989ac40544fefd0120d0
annavalcsev-pixel <anna.valcsev@gmail.com>
2026-07-18 06:55:40 +0200

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'keregapo-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME));
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
