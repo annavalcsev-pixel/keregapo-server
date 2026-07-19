8761c148002364059eb40b61bb367038f2f094cf
annavalcsev-pixel <anna.valcsev@gmail.com>
2026-07-19 14:19:18 +0200

    database.py

--- database.py
+++ database.py
@@ -3,9 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# A Renderben beállított adatbázis URL
 DATABASE_URL = os.environ.get('DATABASE_URL')
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
